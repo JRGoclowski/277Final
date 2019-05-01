d63bcd58e518de91c116510f5679ef8344ee358a
Jonathan Goclowski <JRGoclowski@gmail.com>
2019-05-01 16:57:56 -0700

    Added sub and add functions to Time, continued work on reservation

--- 277 Final/src/edu/csulb/cecs277/DJJJ/Reservation.java
+++ 277 Final/src/edu/csulb/cecs277/DJJJ/Reservation.java
@@ -2,27 +2,131 @@ package edu.csulb.cecs277.DJJJ;
 
 public class Reservation {
 	
-	private Time mStartTime; 
-	private Time mEndTime;
+	private int mMaintenanceTime;
+	private Time mFunctionStartTime; 
+	private Time mFunctionEndTime;
+	private Time mFullStartTime; 
+	private Time mFullEndTime;
 	private Day mDay;
 	private Room mRoom;
 	private Guest mGuest;
+	private Reservation mSetup;
+	private Reservation mGuestReservation;
+	private Reservation mCleanup;
+	
+	public Reservation() {
+		mMaintenanceTime = 0;
+		mFunctionStartTime = null; 
+		mFunctionEndTime = null;
+		mFullStartTime = null; 
+		mFullEndTime = null;
+		mDay = null;
+		mRoom = null;
+		mGuest = null;
+		mSetup = null;
+		mGuestReservation = null;
+		mCleanup = null;
+	}
 	
 	public Reservation(Time start, Time end, Day day, Room room, Guest guest) {
+		mFunctionStartTime = start;
+		mFunctionEndTime = end;
+		mDay = day;
+		mRoom = room;
+		mGuest = guest;
+		setMaintenanceTime();
+		mSetup = MakeSetupReservation(this,true);
+		mCleanup = MakeCleanupReservation(this, false);
+		mGuestReservation = this;
+	}
+	
+	private void setMaintenanceTime() {
+		if (mRoom instanceof SmallPartyRoom) {
+			mMaintenanceTime = 30;
+		}
+		else if (mRoom instanceof KaraokeLounge || mRoom instanceof BilliardsLounge) {
+			mMaintenanceTime = 15;
+		}
+		else {
+			mMaintenanceTime = 60;
+		}
+	}
+	
+	private Reservation MakeSetupReservation (Reservation existing) {
+		Reservation setup = new Reservation();
+		int timeCalc = mFunctionStartTime.getmMinutes() - mMaintenanceTime;
+		if (timeCalc < 0) {
+			
+		}
+		}
+		setup.mFunctionStartTime = mFunctionStartTime;
+		setup.mDay = mDay;
+		
 		
 	}
 	
-	public Reservation (Reservation existing, boolean setup) {
-		int timeToWork = (setup) ? (-1) : (1);
-		if (existing.mRoom instanceof SmallPartyRoom) {
-			 timeToWork *= 30;
-		}
-		else if (existing.mRoom instanceof KaraokeLounge || existing.mRoom instanceof BilliardsLounge) {
-			timeToWork *= 15;
-		}
-		else if ()
-		Time workStartTime;
-		workStartTime = (setup) ? (mStartTime) : (mEndTime);
+	private Reservation MakeCleanupReservation (Reservation existing) {
+		
+	}
+
+	public Time getmGuestStartTime() {
+		return mFunctionStartTime;
+	}
+
+	public void setmGuestStartTime(Time mGuestStartTime) {
+		this.mFunctionStartTime = mGuestStartTime;
+	}
+
+	public Time getmGuestEndTime() {
+		return mFunctionEndTime;
+	}
+
+	public void setmGuestEndTime(Time mGuestEndTime) {
+		this.mFunctionEndTime = mGuestEndTime;
+	}
+
+	public Day getmDay() {
+		return mDay;
+	}
+
+	public void setmDay(Day mDay) {
+		this.mDay = mDay;
+	}
+
+	public Room getmRoom() {
+		return mRoom;
+	}
+
+	public void setmRoom(Room mRoom) {
+		this.mRoom = mRoom;
+	}
+
+	public Guest getmGuest() {
+		return mGuest;
+	}
+
+	public void setmGuest(Guest mGuest) {
+		this.mGuest = mGuest;
+	}
+
+	public int getmMaintenanceTime() {
+		return mMaintenanceTime;
+	}
+
+	public Time getmFullStartTime() {
+		return mFullStartTime;
+	}
+
+	public Time getmFullEndTime() {
+		return mFullEndTime;
+	}
+
+	public Reservation getmGuestReservation() {
+		return mGuestReservation;
+	}
+
+	public Reservation getmCleanup() {
+		return mCleanup;
 	}
 	
 }
--- 277 Final/src/edu/csulb/cecs277/DJJJ/Time.java
+++ 277 Final/src/edu/csulb/cecs277/DJJJ/Time.java
@@ -2,10 +2,15 @@ package edu.csulb.cecs277.DJJJ;
 
 public class Time {
 	
+	public static void main(String args[]) {
+		Time noon = new Time(12, 00);
+		Time morning = new Time (0, 0);
+	}
+	
 	private int mHours;
 	private int mMinutes;
 	private final int MAX_HOURS = 23;
-	private final int MAX_MINUTES = 23;
+	private final int MAX_MINUTES = 60;
 	
 	/**
 	 * Contructor based on military time
@@ -18,7 +23,7 @@ public class Time {
 	}
 	
 	public Time(int hours, int minutes) {
-		if (mHours > MAX_HOURS || mMinutes > MAX_MINUTES) {
+		if (mHours > MAX_HOURS || mMinutes > MAX_MINUTES || minutes < 0 || hours < 0) {
 			mHours = 0;
 			mMinutes = 0;
 		}
@@ -26,6 +31,30 @@ public class Time {
 			mHours = hours;
 			mMinutes = minutes;
 		}
+	}
+	
+	public boolean sub(int subHours, int subMinutes) {
+		if (subHours > MAX_HOURS) { return false; }
+		if (subMinutes > MAX_MINUTES) { return false; }
+		if (subHours > mHours) { return false; }
+		if (subMinutes > mMinutes) {
+			if (subHours > mHours -1) {
+				return false;
+			}
+			mHours -= (subHours + 1);
+			mMinutes = (MAX_MINUTES + (mMinutes - subMinutes));
+			return true;
+		}
+		else {
+			mHours -= subHours;
+			mMinutes -= subMinutes;
+			return true;
+		}
+		
+	}
+	
+	public void add(int addition) {
+		
 	}
 
 	/**
