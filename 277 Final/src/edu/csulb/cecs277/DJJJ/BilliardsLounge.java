11e1f51b0e833dfa9c46502e5a3e65df585c3420
JoeFreedman24 <38093261+JosephFreedman@users.noreply.github.com>
2019-05-06 15:35:10 -0700

    Pre-MealPlan complete

--- 277 Final/src/edu/csulb/cecs277/DJJJ/BilliardsLounge.java
+++ 277 Final/src/edu/csulb/cecs277/DJJJ/BilliardsLounge.java
@@ -1,5 +1,38 @@
 package edu.csulb.cecs277.DJJJ;
 
-public class BilliardsLounge extends Room {
+import java.util.ArrayList;
 
+//generates an BilliardsLounge room from the abstract class Room
+public class BilliardsLounge extends Room implements Cloneable {
+
+	//default constructor
+	public BilliardsLounge() {
+		setRoomCapacity(10);
+		setHourlyCost(25);
+		setRoomDescription("Enclosed lounge with pool table.");
+		setRoomRestrictions("21+ only");
+		addRoomAmenity("access to pool table and cues");
+	}
+
+	//unavailable for this room type
+	public void upgradeTo(String upgrade) {}
+
+	//upgrades the room's meal plan to the given meal plan and adjusts the flat cost appropriately
+	public void upgradeTo(MealPlan newMeal) {
+		if (getRoomMealPlan()==NULL) {
+			addToRoomFlatCost(newMeal.getCost());	//replace getCost method
+			setRoomMealPlan(newMeal);
+		}
+		else if (!(newMeal.getName().equals(getRoomMealPlan().getName()))) {		//----------->Requires placeholder "getName()" to be replaced by the real function to retrieve the name of a meal plan object
+			addToRoomFlatCost(5 * (newMeal.getCost() - getRoomMealPlan().getCost()));	//Requires placeholder "getCost()" to be replaced by the real function to retrieve the cost of a meal plan object
+			setRoomMealPlan(newMeal);
+		}
+	}
+	
+	//creates a clone of the room
+	public Room clone() {
+		KaraokeLounge cloneRoom = (KaraokeLounge) this.clone();
+		return cloneRoom;
+	}
+	
 }
