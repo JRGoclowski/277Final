44d5b89ce8dedffcd3a3433bd034e92f37b0aa6c
JoeFreedman24 <38093261+JosephFreedman@users.noreply.github.com>
2019-05-05 20:33:37 -0700

    Room update
    -created method structure
    -implemented methods
    -requires the currently absent MealPlan class

--- 277 Final/src/edu/csulb/cecs277/DJJJ/Room.java
+++ 277 Final/src/edu/csulb/cecs277/DJJJ/Room.java
@@ -1,11 +1,146 @@
 package edu.csulb.cecs277.DJJJ;
 
-public abstract class Room {
+import java.util.ArrayList;
+
+public abstract class Room extends Cloneable {	//may not need to extend Cloneable
+	
 	private int mNumber;
-	//In order for my code to work, I'm building around the plan
-	//that you are going to have a base room that you will clone
-	//to make the other rooms from. If you could be so kind as 
-	//make it so that the default constructor and base room have 
-	//a room number of 0 plz. 0 will be how I define no specific
-	//room desire
+	private int mCapacity;
+	private String mDescription;
+	private String mRestrictions;
+	private int mHourlyCost;
+	private int mFlatCost;
+	private ArrayList<Date> mDates;
+	private ArrayList<String> mAmenities;
+	private MealPlan mMealPlan;
+	
+	//Default constructor
+	public Room() {
+		mNumber = 0;
+		mCapacity = 0;
+		mDescription = "";
+		mRestrictions = "";
+		mHourlyCost = 0;
+		mFlatCost = 0;
+		mDates = NULL;
+		mMealPlan = NULL;
+	}
+	
+	//------------------This method may not be necessary, revisit------------------------------------
+	//Overloaded constructor for full information
+	public Room(int number, int capacity, String description, int hourlyCost, ArrayList<Date> dates, MealPlan mealPlan) {
+		mNumber = number;
+		mCapacity = capacity;
+		mDescription = description;
+		mRestrictions = "";
+		mHourlyCost = hourlyCost;
+		mDates = dates;
+		mMealPlan = mealPlan;
+		mFlatCost = 0;
+	}
+	
+	//Retrieves the room number
+	public int getRoomNumber() {
+		return mNumber;
+	}
+	
+	//Sets the room number
+	public void setRoomNumber(int num) {
+		mNumber = num;
+	}
+	
+	//Retrieves the room capacity
+	public int getRoomCapacity() {
+		return mCapacity;
+	}
+	
+	//Sets the room capacity
+	public void setRoomCapacity(int capacity) {
+		mCapacity = capacity;
+	}
+	
+	//Retrieves the room description
+	public String getRoomDescription() {
+		return mDescription;
+	}
+	
+	//Sets the room description
+	public void setRoomDescription(String desc) {
+		mDescription = desc;
+	}
+	
+	//Retrieves the room restrictions
+	public String getRoomRestrictions() {
+		return mRestrictions;
+	}
+	
+	//Sets the room restrictions
+	public void setRoomRestrictions(String rest) {
+		mRestrictions = rest;
+	}
+	
+	//Retrieves the room hourly cost
+	public int getHourlyCost() {
+		return mHourlyCost;
+	}
+	
+	//Sets the room hourly cost
+	public void setHourlyCost(int cost) {
+		mHourlyCost = cost;
+	}
+	
+	//Retrieves the room dates
+	public ArrayList<Date> getRoomDates() {
+		return mDates;
+	}
+	
+	//sets the room dates
+	public void setRoomDates(ArrayList<Date> dates) {
+		mDates = dates;
+	}
+	
+	//Retrieves the room meal plan
+	public MealPlan getRoomMealPlan() {
+		return mMealPlan;
+	}
+	
+	//sets the room meal plan
+	public void setRoomMealPlan(MealPlan plan) {
+		mMealPlan = plan;
+	}
+	
+	//adds the provided amenity
+	public void addRoomAmenity(String amenity) {
+		mAmenities.add(amenity);
+	}
+	
+	//adds to the base cost of the room
+	public void addToRoomFlatCost(int cost) {
+		mFlatCost += cost;
+	}
+	
+	//adds to the hourly cost of the room
+	public void addToRoomHourlyCost(int cost) {
+		mHourlyCost += cost;
+	}
+	
+	//Retrieves the room amenities
+	public ArrayList<String> getRoomAmenities() {
+		return mAmenities;
+	}
+	
+	//returns the total cost of the room given hours used
+	public double getTotalCost(double hours) {
+		return mHourlyCost * hours + mFlatCost;
+	}
+	
+	//overloaded method to upgrade amenity
+	public abstract void upgradeTo(String upgrade);
+	
+	//overloaded method to upgrade meal plan
+	public abstract void upgradeTo(MealPlan newMeal);
+	
+	//abstract clone method for subclasses to implement
+	public abstract Object clone();
+	
 }
